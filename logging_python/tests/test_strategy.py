7410c1daebe85d614e7f0b1d5dadd5c426f2df77
Lautaro Coo <lautarocoo@gmail.com>
2021-07-08 20:47:18 -0300

    test for strategy < add remove clear filters > and fixed typo

--- logging_python/tests/test_strategy.py
+++ logging_python/tests/test_strategy.py
@@ -1,4 +1,5 @@
-from logging_python.mutator import Mutator
+from ..mutator import Mutator
+from ..filter import Filter
 from ..strategy import Strategy
 from pytest import raises
 
@@ -16,7 +17,7 @@ def test_strategy_init_filters_mutators():
     assert s.filters() == [] and s.mutators() == []
 
 
-def test_strategy_add_mutaror():
+def test_strategy_add_mutator():
     s = Strategy()
     m1 = Mutator()
     assert s.add_mutator(m1) == True and m1 in s.mutators()
@@ -49,8 +50,34 @@ def test_strategy_clear_mutators():
     assert s.clear_mutators() == True
 
 
+def test_strategy_add_filter():
+    s = Strategy()
+    f1 = Filter()
+    assert s.add_filter(f1) == True and f1 in s.filters()
+    assert s.add_filter(f1)  == False and s.filters().count(f1) == 1
+    f2 = Filter()
+    assert s.add_filter(f2)  == True and f2 in s.filters() and len(s.filters()) == 2 and s.filters().count(f2) == 1
+    assert s.add_filter(None) == False and None not in s.filters()
+    assert s.add_filter() == False
 
 
+def test_strategy_remove_filter():
+    s = Strategy()
+    f1 = Filter()
+    f2 = Filter()
+    s.add_filter(f1)
+    s.add_filter(f2)
+
+    assert s.remove_filter() == False and s.filters() == [f1, f2]
+    assert s.remove_filter(None) == False and s.filters() == [f1, f2]
+    assert s.remove_filter(f1) == True and s.filters() == [f2]
+    assert s.remove_filter(f1) == False and s.filters() == [f2]
 
 
+def test_strategy_clear_filters():
+    s = Strategy()
+    f1 = Filter()
+    s.add_filter(f1)
 
+    assert s.clear_filters() == True and s.filters() == []
+    assert s.clear_filters() == True
